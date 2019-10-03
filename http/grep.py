e4bbcafae757c3a8a7e4a840d93cc371e0f40edb
Your Name <you@example.com>
2019-10-03 22:41:03 +0000

    Getting answer from grep.py.

--- http/grep.py
+++ http/grep.py
@@ -44,80 +44,32 @@ from collections import OrderedDict
 ident = ''
 status = 200
 # Split the URL into its components (project, version, cmd, arg)
-m = search ('^/([^/]*)/([^/]*)/([^/]*)(.*)$', os.environ['SCRIPT_URL'])
+m = search ('^/(.*)$', os.environ['SCRIPT_URL'])
+
+log("Input ", os.environ['SCRIPT_URL'])
+log("Input ", m.group(1))
 
 if m:
-    project = m.group (1)
-    version = m.group (2)
-    cmd = m.group (3)
-    arg = m.group (4)
-
-    log("project version cmd arg", project + version + cmd + arg)
-    
-    if not (project and search ('^[A-Za-z0-9-]+$', project)) \
-    or not (version and search ('^[A-Za-z0-9._-]+$', version)):
-        status = 302
-        location = '/linux/latest/'+cmd+arg
-        cmd = ''
-    if cmd == 'source':
-        path = arg
-        if len (path) > 0 and path[-1] == '/':
-            path = path[:-1]
-            status = 301
-            location = '/'+project+'/'+version+'/source'+path
-        else:
-            mode = 'source'
-            if not search ('^[A-Za-z0-9_/.,+-]*$', path):
-                path = 'INVALID'
-            url = 'source'+path
-    elif cmd == 'ident':
-        ident = arg[1:]
+    cmd = m.group (1)
+    if cmd == 'grep':
         form = cgi.FieldStorage()
-        ident2 = form.getvalue ('i')
-        grep = form.getvalue('grep')
-
-        if grep == 'Yes':
-            f = open("/tmp/grep", "w")
-            f.write("Yes")
-            f.close()
-
-        if ident == '' and ident2:
-            status = 302
-            ident2 = ident2.strip()
-            location = '/'+project+'/'+version+'/ident/'+ident2
-        else:
-            mode = 'ident'
-            if not (ident and search ('^[A-Za-z0-9_-]*$', ident)):
-                ident = ''
-            url = 'ident/'+ident
+        word = form.getvalue ('i')
+        log("Word ", word)
 else:
     status = 404
 
-if status == 301:
-    realprint ('Status: 301 Moved Permanently')
-    realprint ('Location: '+location+'\n')
-    exit()
-elif status == 302:
-    realprint ('Status: 302 Found')
-    realprint ('Location: '+location+'\n')
-    exit()
-elif status == 404:
+if status == 404:
     realprint ('Status: 404 Not Found\n')
     exit()
 
+
 basedir = os.environ['LXR_PROJ_DIR']
+project = "linux"
 os.environ['LXR_DATA_DIR'] = basedir + '/' + project + '/data';
 os.environ['LXR_REPO_DIR'] = basedir + '/' + project + '/repo';
 
-projects = []
-for (dirpath, dirnames, filenames) in os.walk (basedir):
-    projects.extend (dirnames)
-    break
-projects.sort ()
-
 import sys
 sys.path = [ sys.path[0] + '/..' ] + sys.path
-log("sys.path ", sys.path)
 import query
 
 def do_query (*args):
@@ -136,20 +88,15 @@ def do_query (*args):
     del a[-1]
     return a
 
+version = "latest"
 if version == 'latest':
     tag = do_query ('latest')[0]
 else:
     tag = version
 
-f = None
-try:
-    f = open("/tmp/grep", "r")
-    do_grep = f.read()
-except FileNotFoundError:
-    do_grep = "No"
-finally:
-    if (not f is None):
-        f.close()
+url = "source"
+projects = []
+ident = ""
 
 data = {
     'baseurl': '/' + project + '/',
@@ -160,13 +107,8 @@ data = {
     'projects': projects,
     'ident': ident,
     'breadcrumb': '<a class="project" href="'+version+'/source">/</a>',
-    'grep': do_grep
 }
 
-
-f = open("/tmp/grep", "w")
-f.write("No")
-f.close()
 
 lines = do_query ('versions')
 va = OrderedDict()
@@ -209,108 +151,9 @@ for v1k in va:
 
 data['versions'] = v
 
-if mode == 'source':
-    p2 = ''
-    p3 = path.split ('/') [1:]
-    links = []
-    for p in p3:
-        p2 += '/'+p
-        links.append ('<a href="'+version+'/source'+p2+'">'+p+'</a>')
+log("Aloha", "Aloha")
 
-    if links:
-        data['breadcrumb'] += '/'.join (links)
-
-    data['ident'] = ident
-    data['title'] = project.capitalize ()+' source code: '+path[1:]+' ('+tag+') - Bootlin'
-
-    lines = ['null - -']
-
-    type = do_query ('type', tag, path)
-    if len (type) == 1:
-        type = type[0]
-        if type == 'tree':
-            lines += do_query ('dir', tag, path)
-        elif type == 'blob':
-            lines += do_query ('file', tag, path)
-    else:
-        print ('<div class="lxrerror"><h2>This file does not exist.</h2></div>')
-        status = 404
-
-    if type == 'tree':
-        if path != '':
-            lines[0] = 'back - -'
-
-        print ('<div class="lxrtree">')
-        print ('<table><tbody>\n')
-        for l in lines:
-            type, name, size = l.split (' ')
-
-            if type == 'null':
-                continue
-            elif type == 'tree':
-                size = ''
-                path2 = path+'/'+name
-                name = name
-            elif type == 'blob':
-                size = size+' bytes'
-                path2 = path+'/'+name
-            elif type == 'back':
-                size = ''
-                path2 = os.path.dirname (path[:-1])
-                if path2 == '/': path2 = ''
-                name = 'Parent directory'
-
-            print ('  <tr>\n')
-            print ('    <td><a class="tree-icon icon-'+type+'" href="'+version+'/source'+path2+'">'+name+'</a></td>\n')
-            print ('    <td><a tabindex="-1" class="size" href="'+version+'/source'+path2+'">'+size+'</a></td>\n')
-            print ('  </tr>\n')
-
-        print ('</tbody></table>', end='')
-        print ('</div>')
-
-    elif type == 'blob':
-        del (lines[0])
-
-        import pygments
-        import pygments.lexers
-        import pygments.formatters
-        links = []
-        code = StringIO()
-
-        def keep_links(match):
-            links.append (match.group (1))
-            g = match.group(1)
-            return '__KEEPLINKS__' + str(len(links))
-
-        def replace_links(match):
-            i = links[int (match.group (1)) - 1]
-            return '<a href="'+version+'/ident/'+i+'">'+i+'</a>'
-
-        for l in lines:
-            l = sub ('\033\[31m(.*?)\033\[0m', keep_links, l)
-            l = sub ('\033\[32m', '', l)
-            l = sub ('\033\[33m', '', l)
-            l = sub ('\033\[0m', '', l)
-            code.write (l + '\n')
-
-        code = code.getvalue()
-
-        try:
-            lexer = pygments.lexers.guess_lexer_for_filename (path, code)
-        except:
-            lexer = pygments.lexers.get_lexer_by_name ('text')
-
-        lexer.stripnl = False
-        formatter = pygments.formatters.HtmlFormatter (linenos=True, anchorlinenos=True)
-        result = pygments.highlight (code, lexer, formatter)
-
-        result = sub ('href="#-(\d+)', 'name="L\\1" id="L\\1" href="'+version+'/source'+path+'#L\\1', result)
-        result = sub ('__KEEPLINKS__(\d+)', replace_links, result)
-
-        print ('<div class="lxrcode">' + result + '</div>')
-
-
-elif mode == 'ident':
+if cmd == 'ident':
     if (data['grep'] == "No"):
         data['title'] = project.capitalize ()+' source code: '+ident+' identifier ('+tag+') - Bootlin'
 
@@ -364,8 +207,6 @@ elif mode == 'ident':
                 print ('<h2>Identifier not used</h2>')
                 status = 404
         print ('</div>')
-    elif (data['grep'] == "Yes"):
-        raise Exception
 else:
     print ('Invalid request')
 
@@ -375,7 +216,7 @@ if status == 404:
 import jinja2
 loader = jinja2.FileSystemLoader (os.path.join (os.path.dirname (__file__), '../templates/'))
 environment = jinja2.Environment (loader=loader)
-template = environment.get_template ('layout.html')
+template = environment.get_template ('layout_grep.html')
 
 realprint ('Content-Type: text/html;charset=utf-8\n')
 data['main'] = outputBuffer.getvalue()
